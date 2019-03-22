b4e251b2ae9b5289fe3a23f828399c9189acb3d3
wangxinyu4027789 <394294920@qq.com>
2019-03-22 14:34:22 +0800

    Update GaoDeLocation.java
    修改默认配置

--- src/android/GaoDeLocation.java
+++ src/android/GaoDeLocation.java
@@ -104,17 +104,17 @@ public class GaoDeLocation extends CordovaPlugin {
      */
     private AMapLocationClientOption getDefaultOption() {
         AMapLocationClientOption mOption = new AMapLocationClientOption();
-        mOption.setLocationMode(AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
-        mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        mOption.setInterval(2000);//可选，设置定位间隔。默认为2秒
-        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
-        mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
-        mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
-        AMapLocationClientOption.setLocationProtocol(AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
-        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
-        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
-        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+             mOption.setLocationMode(AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
+             mOption.setGpsFirst(true);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+             mOption.setHttpTimeOut(10000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+             mOption.setInterval(1000);//可选，设置定位间隔。默认为2秒
+             mOption.setNeedAddress(false);//可选，设置是否返回逆地理地址信息。默认是true
+             mOption.setOnceLocation(true);//可选，设置是否单次定位。默认是false
+             mOption.setOnceLocationLatest(true);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+             AMapLocationClientOption.setLocationProtocol(AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
+             mOption.setSensorEnable(true);//可选，设置是否使用传感器。默认是false
+             mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+             mOption.setLocationCacheEnable(false); //可选，设置是否使用缓存定位，默认为true
         return mOption;
     }
 
@@ -130,7 +130,7 @@ public class GaoDeLocation extends CordovaPlugin {
                     //解析定位结果
                     //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
                     if (location.getErrorCode() == 0) {
-                        json.put("status", "定位成功");
+                        json.put("status", 200);
                         //定位类型
                         json.put("type", location.getLocationType());
                         //纬度
@@ -163,7 +163,7 @@ public class GaoDeLocation extends CordovaPlugin {
                         //兴趣点
                         json.put("time", location.getTime());
                     } else {
-                        json.put("status", "定位失败");
+                        json.put("status", 500);
                         json.put("errcode", location.getErrorCode());
                         json.put("errinfo", location.getErrorInfo());
                         json.put("detail", location.getLocationDetail());
